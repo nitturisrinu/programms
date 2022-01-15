78fe8d51d348f271f4f566ab7874dda6e35c45bc
nitturisrinu <nsrinu540@gmail.com>
2022-01-15 23:44:32 +0530

    newly added

---
+++ corejavapro/corejava/src/javabasics/Demo.java
@@ -0,0 +1,11 @@
+package javabasics;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+	int a=10;  
+	float f=a;  
+	System.out.println(a);  
+	System.out.println(f);  
+	}
+	}  
---
+++ corejavapro/corejava/src/javabasics/Demo1.java
@@ -0,0 +1,11 @@
+package javabasics;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		
+	int a=100;  
+	float f=a;  
+	System.out.println(a);  
+	System.out.println(f);  
+	}
+}
